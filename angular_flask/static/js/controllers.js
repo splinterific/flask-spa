fd23646ec74588c6a624bf84c099feda406d1d16
Splinterific <markgpreston@gmail.com>
2016-07-24 09:46:03 +0100

    init

---
+++ angular_flask/static/js/controllers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/* Controllers */
+
+function IndexController($scope) {
+
+}
+
+function AboutController($scope) {
+
+}
+
+function MarkController($scope) {
+
+}
+
+function PostListController($scope, Post) {
+	var postsQuery = Post.get({}, function(posts) {
+		$scope.posts = posts.objects;
+	});
+}
+
+function PostDetailController($scope, $routeParams, Post) {
+	var postQuery = Post.get({ postId: $routeParams.postId }, function(post) {
+		$scope.post = post;
+	});
+}
